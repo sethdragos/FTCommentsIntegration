23c6c10a2596b120ded00f504e19f30b747e1f0d
Vallach <vallach@yahoo.ro>
2013-11-18 15:56:52 +0200

    added a few basic classes

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class Constants {
+	
+	public static final String GRID_HUB = "http://localhost:4444/wd/hub";
+
+}
---
+++ src/test/java/mainTests/mainTests.java
@@ -0,0 +1,39 @@
+package mainTests;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class mainTests {
+	
+	private WebDriver driver;
+	private String baseURL = ("http://jwt-app.herokuapp.com/");
+
+	@BeforeTest
+	public void createDriver() {
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.get(baseURL);
+	}
+
+	@AfterTest
+	public void tidyUp() {
+		driver.quit();
+	}
+
+	@Test (testName="VerifyPageTitle")
+	public void VerifyPageTitle() {
+		
+		String expectedTitle = "Livefyre test page - FT.com";
+		Assert.assertEquals(this.driver.getTitle(), expectedTitle);
+		
+	}
+
+}
