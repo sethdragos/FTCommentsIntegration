0840bbc569d4399d7710ef55ad5a84f0d6b3bf2c
Vallach <vallach@yahoo.ro>
2013-12-03 14:22:48 +0200

    Added BetaFeatures Selenium IDE scripts

--- src/main/java/pageObjects/BetaFeaturesPage.java
+++ src/main/java/pageObjects/BetaFeaturesPage.java
@@ -1,5 +1,41 @@
 package pageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
 public class BetaFeaturesPage {
+	
+	private WebDriver driver;
+	
+	@FindBy (xpath = ".//*[@id='beta-features-dashboard']/div/div/div[1]/div/div/div/div/a[1]")
+	private WebElement BetaFeatureOptInBtn;
+	
+	@FindBy (xpath = ".//*[@id='beta-features-dashboard']/div/div/div[1]/div/div/div/div/a[2]")
+	private WebElement BetaFeatureOptOutBtn;
+	
+	@FindBy (xpath = ".//*[@id='beta-features-dashboard']/div/div/div[2]/div/div/div[1]/div/div/a[1]")
+	private WebElement LiveFyreOptInBtn;
+	
+	@FindBy (xpath = ".//*[@id='beta-features-dashboard']/div/div/div[2]/div/div/div[1]/div/div/a[2]")
+	private WebElement LiveFyreOptOutBtn;
+	
+	@FindBy (xpath = ".//*[@id='beta-features-dashboard']/div/div/div[2]/div/div/div[2]/div/div/a[1]")
+	private WebElement LiveFyreAutoLoginOptInBtn;
+	
+	@FindBy (xpath = ".//*[@id='beta-features-dashboard']/div/div/div[2]/div/div/div[2]/div/div/a[2]")
+	private WebElement LiveFyreAutoLoginOptOUTBtn;
+	
+	public BetaFeaturesPage(WebDriver _driver) {
+		
+		this.driver = _driver;
+		PageFactory.initElements(driver, this);
+		
+		String expectedTitle = "Internal beta features";
+		Assert.assertEquals(this.driver.getTitle(), expectedTitle);
+		
+	}
 
 }
